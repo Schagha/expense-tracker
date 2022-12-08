4c28dfff1988151843081eee2b71d514f150ca8f
Shaghay <102187856+Shaghay@users.noreply.github.com>
2022-12-08 20:40:51 +0100

    dynamic list of expenses

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -64,7 +64,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className="new-espense__control">
           <lable>Date</lable>
-          <input type='date' min='2019-01-01' max='2022-12-31' 
+          <input type='date' min='2020-01-01' max='2023-12-31' 
           value={enteredDate} 
           onChange={dateChangeHandler} />
         </div>
